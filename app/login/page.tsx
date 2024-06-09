d265559b9d9cc2596f383f690d72a58365a89e02
Zhansaya Sugiralieva <zhansaya@localhost.localdomain>
2024-06-09 22:32:46 +0500

    translate form labels into english

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -37,29 +37,29 @@ const Login: React.FC = () => {
         <div className='bg-white flex items-center justify-center min-h-screen'>
         <form onSubmit={handleLogin} className='bg-gray-100 p-8 rounded shadow-md w-full max-w-sm'>
             <div className='mb-4'>
-                <label className='block text-gray-700 text-sm font-bold mb-2' htmlFor="username">Имя пользователя:</label>
+                <label className='block text-gray-700 text-sm font-bold mb-2' htmlFor="username">Username:</label>
                 <input
                     className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:input-focused'
                     id="username"
                     type="text"
-                    placeholder="Имя пользователя"
+                    placeholder="Username"
                     value={username}
                     onChange={(e) => setUsername(e.target.value)}
                 />
             </div>
             <div className='mb-6'>
-                <label className='block text-gray-700 text-sm font-bold mb-2' htmlFor="password">Пароль:</label>
+                <label className='block text-gray-700 text-sm font-bold mb-2' htmlFor="password">Password:</label>
                 <input
                     className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:input-focused'
                     id="password"
                     type="password"
-                    placeholder="Пароль"
+                    placeholder="Password"
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
                 />
             </div>
             <div className='flex items-center justify-between'>
-            <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline' type="submit">Войти</button>
+            <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline' type="submit">Login</button>
             </div>
         </form>
         </div>
